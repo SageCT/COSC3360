60bb248f2535da0d9879cf31fd588b2df824740a
SageCT <egas6554@gmail.com>
2023-01-17 17:03:42 -0600

    Created base files for Assignment 1

---
+++ HW1/huffmanTree.h
@@ -0,0 +1,10 @@
+#ifndef HUFFMANTREE_H
+#define HUFFMANTREE_H
+
+class huffmanTree {
+ public:
+  huffmanTree();
+  ~huffmanTree();
+};
+
+#endif HUFFMANTREE_H
---
+++ HW1/main.cpp
@@ -0,0 +1,8 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "huffmanTree.h"
+
+int main() { return 0; }
