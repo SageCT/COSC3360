61f73d14b092d062b6345516264f3ba1d3ea8dd5
SageCT <egas6554@gmail.com>
2023-01-18 15:45:40 -0600

    Started work in main to organize data from least to greatest

--- HW1/huffmanTree.h
+++ HW1/huffmanTree.h
@@ -1,6 +1,9 @@
 #ifndef HUFFMANTREE_H
 #define HUFFMANTREE_H
 
+#include <map>
+
+using namespace std;
 struct node {
   char data;
   int freq;
@@ -8,15 +11,26 @@ struct node {
   node *right;
 };
 
-
 class huffmanTree {
- public:
-  huffmanTree();
+public:
+  int size;
+  node *head;
+  huffmanTree() : size(0), head(nullptr){};
+  huffmanTree(map<char, int> &m) : size(m.size()), head(nullptr) {
+    buildHuffmanTree(m);
+  };
   ~huffmanTree();
+  void buildHuffmanTree(map<char, int> &m);
 };
 
-huffmanTree::huffmanTree(){
-
+void huffmanTree::buildHuffmanTree(map<char, int> &m) {
+  auto it = m.end();
+  head = new node;
+  head->data = it->first;
+  head->freq = it->second;
+  it--;
+  for (it; it != m.begin(); it--)
+    cout << it->first << " " << it->second << endl;
 }
 
 #endif
--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,8 +1,26 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
 #include "huffmanTree.h"
 
-int main() { return 0; }
+using namespace std;
+
+int main() {
+  string in;
+  char c;
+  int n;
+  map<char, int> m;
+
+  getline(cin, in);
+  if (isalpha(in.at(0)))
+    c = in.at(0);
+  else
+    c = in.at(1);
+
+  m[c] = n;
+
+  return 0;
+}
