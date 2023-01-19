354786c90784f024d5da71c7b2ae6968055765eb
SageCT <egas6554@gmail.com>
2023-01-19 12:48:32 -0600

    Further work on main.cpp input handling

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 int main() {
   string in, toAdd;
-  map<char, vector<char>> codes;
   int counter = 0;
-  map<int, char> m;
+  map<int, vector<int>> codes;
+  map<char, int> m;
   cin >> in;
   cin >> in;
   in.clear();
@@ -21,12 +21,32 @@ int main() {
   // Gets line from STDIN, and checks if the first character is a digit,
   // if not a digit adds to map
   getline(cin, in);
-  while (!isdigit(in.at(0))) {
-    if (isalpha(in.at(0)))
-      m.insert(pair<int, char>(in.at(0), in.at(3))), counter++;
+  while (isalpha(in.at(0))) {
+    m.insert(pair<char, int>(in.at(0), in.at(3))), counter++;
 
-    in.clear();
     getline(cin, in);
+  }
+
+  if (isdigit(in.at(0))) {
+    toAdd = "";
+    vector<int> temp;
+    // Find the code in the string
+    toAdd += in.substr(0, in.find(" ") - 1);
+    // Erase the code from the input string
+    in.erase(0, in.find(" ") + 1);
+
+    // While the input string is not empty push back the digits until you find a
+    // space or the end of the string
+    while (in.size() > 0) {
+      if (in.find(" ") == string::npos) {
+        temp.push_back(stoi(in));
+      } else if (in.find(" ") != string::npos)
+        temp.push_back(stoi(in.substr(0, in.find(" ") - 1)));
+    }
+    temp.push_back(stoi());
+
+    while (getline(cin, in)) {
+    }
   }
 
   // huffmanTree tree(m);
