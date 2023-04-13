baa9acc8ccb13e45082315cc36c64cc5fa0a38b7
SageCT <egas6554@gmail.com>
2023-04-13 18:04:20 -0500

    Transfer to desktop

--- NOTES/2023.04.02 Catchup EXAM2/main.cpp
+++ NOTES/2023.04.02 Catchup EXAM2/main.cpp
@@ -11,10 +11,41 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-using namespace std;
-int main() {
-  string s;
-  getline(cin, s);
 
-    return 0;
+#include "huffmanTree.h"
+
+using namespace std;
+
+struct ThreadArgs {
+  int id;
+  pthread_cond_t *waitTurn;
+  pthread_mutex_t *mutex;
+};
+
+void *addOneHundred(void *id) {
+  int *id_ = (int *)id;
+  for (int i = 0; i < 100; i++)
+    cout << *id_ << " " << i << endl;
+}
+
+void addOneHundred(int id, pthread_mutex_t &mutex) {
+  for (int i = 0; i < 100; i++) {
+    cout << id << " " << i << endl;
+  }
+}
+
+int main() {
+  pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+  int one = 1, two = 2;
+  pthread_mutex_t mutex;
+
+  pthread_t t1, t2;
+  ThreadArgs *args1 = new ThreadArgs;
+  args1->id = 1;
+  args1->mutex = &mutex;
+
+  pthread_create(&t1, NULL, addOneHundred, (void *)&args1);
+
+  return 0;
 }
